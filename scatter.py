1d409eb8dfbc95da216b8d1c21250cfc4390cb78
Sruthy-Sreekumar <148990178+Sruthy-Sreekumar@users.noreply.github.com>
2024-10-23 11:26:09 +0530

    scatter plot

---
+++ scatter.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+maths=[89,92,18,89,100,80,60,100,80,34]
+science=[35,79,79,48,100,88,32,45,20,30]
+plt.scatter(maths,science)
+plt.xlabel("Maths")
+plt.ylabel("science")
+plt.show()
+
