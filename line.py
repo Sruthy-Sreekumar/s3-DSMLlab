d41840f442fb732e20ff8d8602a60e468aa84098
Sruthy-Sreekumar <148990178+Sruthy-Sreekumar@users.noreply.github.com>
2024-10-23 11:16:24 +0530

    line

---
+++ line.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x=np.array([1,2,6])
+y=np.array([2,10,12])
+plt.plot(x,y)
+plt.title("Line diagram")
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+plt.show()
